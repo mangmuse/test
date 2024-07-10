8d4825cd41ca56aec5bfc56cae5dbc0a79057db5
mangmuse <mangmangmuse@gmail.com>
2024-07-11 00:41:00 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ const nextConfig = {
   async redirects() {
     return [{ source: "/", destination: "/todos/today", permanent: true }];
   },
+
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
