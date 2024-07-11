1d795a7e4e6b1040c8cf5ec536fab0e281b6be70
mangmuse <mangmangmuse@gmail.com>
2024-07-11 11:55:20 +0900

    asd

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,23 +1,26 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  const supabase = createClient();
+// export async function POST(req: NextRequest) {
+//   const supabase = createClient();
 
-  console.log("Webhook POST request received");
+//   console.log("Webhook POST request received");
 
-  const body = await req.text(); // Google은 웹훅 데이터를 텍스트 형식으로 보냅니다.
+//   const body = await req.text();
 
-  console.log("Webhook data:", body);
+//   console.log("Webhook data:", body);
 
-  const { data, error } = await supabase
-    .from("webhook_logs")
-    .insert([{ request_body: body }]);
+//   const { data, error } = await supabase
+//     .from("webhook_logs")
+//     .insert([{ request_body: body }]);
 
-  if (error) {
-    console.error("Error saving log to Supabase:", error);
-    return NextResponse.json({ error: "Failed to save log" }, { status: 500 });
-  }
+//   if (error) {
+//     console.error("Error saving log to Supabase:", error);
+//     return NextResponse.json({ error: "Failed to save log" }, { status: 500 });
+//   }
 
-  return NextResponse.json({ message: "POST response", data });
+//   return NextResponse.json({ message: "POST response", data });
+// }
+export async function POST() {
+  return NextResponse.json("포스트");
 }
