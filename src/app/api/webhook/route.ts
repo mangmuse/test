3b5504675b71cbc5b58236afaa30fc8aa525810a
mangmuse <mangmangmuse@gmail.com>
2024-07-11 11:27:53 +0900

    asd

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,32 +1,12 @@
-import { NextApiRequest, NextApiResponse } from "next";
-// import { watchCalendar } from "../../../utils/googleCalendar";
-import { createClient } from "@/utils/supabase/server";
-import { watchCalendar } from "@/utils/googleCalendar";
+import { NextRequest, NextResponse } from "next/server";
 
-export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === "POST") {
-    const { calendarId } = req.body;
+export async function GET(req: NextRequest) {
+  console.log("webhook get");
+  return NextResponse.json({ message: "GET response" });
+}
 
-    const supabase = createClient();
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.getSession();
+export async function POST(req: NextRequest) {
+  console.log("webhook post");
 
-    if (error || !session) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
-
-    const accessToken = session.provider_token;
-    if (!accessToken) throw new Error();
-    try {
-      const watchResponse = await watchCalendar(accessToken, calendarId);
-      res.status(200).json(watchResponse);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Failed to setup webhook" });
-    }
-  } else {
-    res.status(405).send("Method Not Allowed");
-  }
-};
+  return NextResponse.json({ message: "POST response" });
+}
